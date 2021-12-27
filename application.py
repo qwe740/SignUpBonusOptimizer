381904c0fef1041cd8786adc09184c3508e3b2af
Austin Bervig <austinberviginc@gmail.com>
2021-12-27 11:52:51 -0600

    Fix bugs

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 from flask import Flask, app, request
+from flask_cors import CORS
 import json
 from bonusOptimization import HelperFunctions, BonusOptimizer
 
 application = Flask(__name__)
+CORS(application)
 
 @application.route('/',methods=['GET'])
 def index():
@@ -26,7 +28,7 @@ Calculate EV given fair odds: This calculation returns the expected value (based
                                 the actual implied probability of winning the bet)
 '''
 
-@application.route('/calculations/calculateVig',methods=['GET'])
+@application.route('/calculations/calculateVig', methods=['GET'])
 def calculateVig():
     params = request.args['odds']
     params = params.split(',')
@@ -36,11 +38,12 @@ def calculateVig():
 
 @application.route('/calculations/convertOdds', methods=['GET'])
 def convertOdds():
-    params = json.loads(request.args)
-    odds = int(params['odds'])
+    params = request.args
     if params['format'] == 'american':
+        odds = int(params['odds'])
         new_odds = HelperFunctions.americanToFractional(odds)
     else:
+        odds = float(params['odds'])
         new_odds = HelperFunctions.fractionalToAmerican(odds)
     return {"value":new_odds}
 
@@ -54,38 +57,38 @@ def calculateFairOdds():
 
 @application.route('/calculations/calculateOtherOddsGivenVig', methods=['GET'])
 def calculateOtherOddsGivenVig():
-    params = json.loads(request.args)
+    params = request.args
     odds = int(params['odds'])
-    vig = params['vig']
+    vig = float(params['vig'])
     return {"value": HelperFunctions.otherOddsGivenVig(odds,vig)}
 
 @application.route('/calculations/calculateEVGivenFairOdds', methods=['GET'])
 def calculateEVGivenFairOdds():
-    params = json.loads(request.args)
-    odds = params['odds']
-    fairOdds = params['fairOdds']
+    params = request.args
+    odds = int(params['odds'])
+    fairOdds = int(params['fairOdds'])
     return {"value": HelperFunctions.EVgivenFairOdds(odds,fairOdds)}
 
 @application.route('/betOptimizer/RiskFreeBet', methods=['GET'])
 def getRFBetEV():
-    params = json.loads(request.args)
-    stake = params['stake']
-    odds1 = params['odds1']
-    odds2 = params['odds2']
+    params = request.args
+    stake = float(params['stake'])
+    odds1 = int(params['odds1'])
+    odds2 = int(params['odds2'])
     try:
-        freeBetConversion = params['freeBetConversion']
+        freeBetConversion = float(params['freeBetConversion'])
     except:
         freeBetConversion = 0.65
     return BonusOptimizer.RFBetEV(stake, odds1, odds2, freeBetConversion)
 
 @application.route('/betOptimizer/BonusMatch', methods=['GET'])
 def getBonusMatchEV():
-    params = json.loads(request.args)
-    stake = params['stake']
-    odds1 = params['odds1']
-    odds2 = params['odds2']
+    params = request.args
+    stake = float(params['stake'])
+    odds1 = int(params['odds1'])
+    odds2 = int(params['odds2'])
     try:
-        freeBetConversion = params['freeBetConversion']
+        freeBetConversion = float(params['freeBetConversion'])
     except:
         freeBetConversion = 0.65
 
